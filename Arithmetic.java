10457d80af615301e6caefdebf946dc6a0f40420
Wes Breisch <wabreisch@quinnipiac.edu>
2015-10-06 14:34:35 -0400

    Adds multiply functionality

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,7 +1,13 @@
 public class Arithmetic {
 
   public static void main(String[] args) {
-    
+    int a = 5;
+    int b = 15;
+    printf("%d * %d = %d", a, b, multiply(a,b));
+  }
+
+  public static int multiply(int a, int b) {
+    return a * b;
   }
 
 }
