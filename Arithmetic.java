573434f9c401ae8a20e2100e3989895f9051def0
Wes Breisch <wabreisch@quinnipiac.edu>
2015-10-06 14:37:27 -0400

    Adds subtraction functionality

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,7 +1,13 @@
 public class Arithmetic {
 
   public static void main(String[] args) {
-    
+    int a = 25;
+    int b = 20;
+    printf("%d - %d = %d", a, b, subtract(a,b));
+  }
+
+  public static int subtract(int a, int b) {
+    return a - b;
   }
 
 }
