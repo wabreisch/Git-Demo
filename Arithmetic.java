578c4592582136110b62aeb6206ae987566eecb9
Wes Breisch <wabreisch@quinnipiac.edu>
2015-10-06 14:38:40 -0400

    Adds division functionality

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,7 +1,13 @@
 public class Arithmetic {
 
   public static void main(String[] args) {
-    
+    int a = 25;
+    int b = 5;
+    printf("%d / %d = %d\n", a, b, divide(a,b));
+  }
+
+  public static int divide(int a, int b) {
+    return a / b;
   }
 
 }
