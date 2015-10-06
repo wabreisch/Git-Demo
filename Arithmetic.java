3ad3b6e4b8ecbbf24fca1674a6b1fc3438ce1050
Wes Breisch <wabreisch@quinnipiac.edu>
2015-10-06 14:32:40 -0400

    Adds addition funcitonality

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,7 +1,13 @@
 public class Arithmetic {
 
   public static void main(String[] args) {
-    
+    int a = 10;
+    int b = 20;
+    printf("%d + %d = %d\n", a, b, add(a,b));
+  }
+
+  public static int add(int a, int b) {
+    return a + b;
   }
 
 }
