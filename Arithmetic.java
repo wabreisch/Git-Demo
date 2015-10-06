a0d5f5d1b96d636f739f2c7475bacd13df10c9d7
Wes Breisch <wabreisch@quinnipiac.edu>
2015-10-06 14:39:05 -0400

    Fix

--- Arithmetic.java
+++ Arithmetic.java
@@ -3,7 +3,7 @@ public class Arithmetic {
   public static void main(String[] args) {
     int a = 5;
     int b = 15;
-    printf("%d * %d = %d", a, b, multiply(a,b));
+    printf("%d * %d = %d\n", a, b, multiply(a,b));
   }
 
   public static int multiply(int a, int b) {
